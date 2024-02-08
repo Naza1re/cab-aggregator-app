4212d7414d14c9ff0ce1c22f1e1f6ac55dbc22a3
Naza1re <mcarim@mail.ru>
2024-02-08 13:20:54 +0300

    add : eureka server configuration

--- payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -2,14 +2,16 @@ package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class PaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
 
 }
