d89675fc1bd71689276cc7a06630b4cfd01abdd5
Naza1re <mcarim@mail.ru>
2024-02-10 20:22:23 +0300

    initial commit for end-2-end

---
+++ end-2-end/src/main/java/com/example/end2end/End2EndApplication.java
@@ -0,0 +1,13 @@
+package com.example.end2end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class End2EndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(End2EndApplication.class, args);
+	}
+
+}
