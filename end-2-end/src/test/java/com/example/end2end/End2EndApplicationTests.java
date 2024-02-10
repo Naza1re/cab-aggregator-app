36f150aed133e4a311d811e20ca8b1b311d3eb81
Naza1re <mcarim@mail.ru>
2024-02-10 20:22:23 +0300

    initial commit for end-2-end

---
+++ end-2-end/src/test/java/com/example/end2end/End2EndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.end2end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class End2EndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
