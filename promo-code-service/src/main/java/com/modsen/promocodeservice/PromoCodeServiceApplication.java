1b40b9e45adbfb664eb606f1976fa91133fab290
Naza1re <mcarim@mail.ru>
2024-02-20 18:22:03 +0300

    git init for promoCode service

---
+++ promo-code-service/src/main/java/com/modsen/promocodeservice/PromoCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.promocodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoCodeServiceApplication.class, args);
+	}
+
+}
