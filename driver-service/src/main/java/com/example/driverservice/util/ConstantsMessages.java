b82a94008db6631db68ba033c49aa22c26df65db
Naza1re <mcarim@mail.ru>
2024-02-28 01:43:29 +0300

    add : logs for driver-service

--- driver-service/src/main/java/com/example/driverservice/util/ConstantsMessages.java
+++ driver-service/src/main/java/com/example/driverservice/util/ConstantsMessages.java
@@ -6,5 +6,18 @@ import lombok.experimental.UtilityClass;
 public class ConstantsMessages {
 
     public static final String NEW_AVAILABLE_DRIVERS = "New Drivers available";
-    public static final String DRIVERS_NOT_AVAILABLE = "Drivers not available";
+     public static final String DRIVERS_NOT_AVAILABLE = "Drivers not available";
+     public static final String GET_DRIVER_BY_ID_LOG_MESSAGE = "Getting driver with id : %s";
+     public static final String GET_DRIVER_LIST_LOG_MESSAGE = "Getting driver list";
+     public static final String GET_DRIVER_AVAILABLE_LIST_LOG_MESSAGE = "Getting available driver list";
+     public static final String UPDATE_DRIVER_WITH_ID = "Updating driver with id : %s";
+     public static final String GETTING_DRIVER_PAGE = "Getting driver page";
+     public static final String CREATE_DRIVER_WITH_ID = "Creating driver with id : %s";
+     public static final String DELETE_DRIVER_WITH_ID = "Deleting driver with id : %s";
+     public static final String DRIVER_WITH_MAIL_ALREADY_EXIST = "Driver with mail : %s already exist";
+     public static final String DRIVER_WITH_PHONE_ALREADY_EXIST = "Driver with mail : %s already exist";
+     public static final String DRIVER_WITH_NUMBER_ALREADY_EXIST = "Driver with car number : %s already exist";
+     public static final String DRIVER_WITH_ID_NOT_FOUND =  "Driver with id :  %s not found";
+     public static final String CHANGING_STATUS_DRIVER_WITH_ID = "Changing driver status with id : %s ";
+
 }
