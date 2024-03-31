e273ef84b422e6e4dd165be5f38393c4957b4857
Naza1re <mcarim@mail.ru>
2024-03-31 23:16:41 +0300

    add : caching for driver service

--- driver-service/src/main/java/com/example/driverservice/DriverServiceApplication.java
+++ driver-service/src/main/java/com/example/driverservice/DriverServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.driverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableCaching
 public class DriverServiceApplication {
 
     public static void main(String[] args) {
