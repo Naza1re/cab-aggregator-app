1adf29836d35651228a87353aeae73abcebf88a3
Naza1re <mcarim@mail.ru>
2024-03-31 23:18:33 +0300

    add : caching for promo-code service

--- promo-code-service/src/main/java/com/modsen/promocodeservice/PromoCodeServiceApplication.java
+++ promo-code-service/src/main/java/com/modsen/promocodeservice/PromoCodeServiceApplication.java
@@ -2,10 +2,12 @@ package com.modsen.promocodeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCaching
 public class PromoCodeServiceApplication {
 
 	public static void main(String[] args) {
