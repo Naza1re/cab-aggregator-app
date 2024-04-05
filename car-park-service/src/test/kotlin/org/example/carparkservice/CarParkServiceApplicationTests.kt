837a3e36ceca576084134107e402e004bef7a3f2
Naza1re <mcarim@mail.ru>
2024-04-05 22:47:48 +0300

    add application.yml

---
+++ car-park-service/src/test/kotlin/org/example/carparkservice/CarParkServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.carparkservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarParkServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
