b07e60233b9ba2d5a9e742d87393535c4070cfae
Naza1re <mcarim@mail.ru>
2024-04-15 22:20:23 +0300

    add : price-calculator-service

--- price-calculation-service/src/main/java/org/example/pricecalculationservice/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.pricecalculationservice.repository;
-
-import org.example.pricecalculationservice.model.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
-}
