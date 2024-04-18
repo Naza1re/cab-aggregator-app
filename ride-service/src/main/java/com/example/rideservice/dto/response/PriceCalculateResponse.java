ed687cf0f7ed937c4588a73508b785f24ca863e9
Naza1re <mcarim@mail.ru>
2024-04-18 20:09:35 +0300

    move getting price by weather to price-service

---
+++ ride-service/src/main/java/com/example/rideservice/dto/response/PriceCalculateResponse.java
@@ -0,0 +1,21 @@
+package com.example.rideservice.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PriceCalculateResponse {
+    private BigDecimal price;
+    private LocalDateTime timestamp;
+
+}
+
